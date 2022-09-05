f673d3825329ceef93519b369391aab7efef2cdb
Eitheages <Eitheages725@sjtu.edu.cn>
2022-09-05 15:05:27 +0800

    update

--- main.cpp
+++ main.cpp
@@ -242,7 +242,7 @@ void VGG11(engine::kind engine_kind) {
     auto conv2_src_memory = pool1_dst_memory;
     if (conv2_pd.src_desc() != conv2_src_memory.get_desc()) {
         conv2_src_memory = memory(conv2_pd.src_desc(), eng);
-        net_fwd.push_back(reorder(conv2_src_memory, conv2_src_memory));
+        net_fwd.push_back(reorder(pool1_dst_memory, conv2_src_memory));
         net_fwd_args.push_back({{DNNL_ARG_FROM, pool1_dst_memory},
                 {DNNL_ARG_TO, conv2_src_memory}});
     }
@@ -313,6 +313,282 @@ void VGG11(engine::kind engine_kind) {
     net_fwd_args.push_back({{DNNL_ARG_SRC, relu2_dst_memory},
             {DNNL_ARG_DST, pool2_dst_memory}, // delay putting DST until reorder (if needed)
             {DNNL_ARG_WORKSPACE, pool2_workspace_memory}});
+
+    // VGG11: block 3-1: conv3
+    // {batch, 128, 56, 56} -> {batch, 256, 56, 56}
+    // kernel: {3, 3}; strides: {1, 1}; padding: {1, 1}
+    memory::dims conv3_src_tz = {N, 128, 56, 56};
+    memory::dims conv3_weights_tz = {256, 128, 3, 3};
+    memory::dims conv3_bias_tz = {256};
+    memory::dims conv3_dst_tz = {N, 256, 56, 56};
+    memory::dims conv3_strides = {1, 1};
+    memory::dims conv3_padding = {1, 1};
+
+    std::vector<float> conv3_weights(product(conv3_weights_tz));
+    std::vector<float> conv3_bias(product(conv3_bias_tz));
+
+    // initializing non-zero values for weights and bias
+    for (size_t i = 0; i < conv3_weights.size(); ++i)
+        conv3_weights[i] = sinf((float)i);
+    for (size_t i = 0; i < conv3_bias.size(); ++i)
+        conv3_bias[i] = sinf((float)i);
+
+    // create memory for user data
+    auto conv3_user_weights_memory
+            = memory({{conv3_weights_tz}, dt::f32, tag::oihw}, eng);
+    write_to_dnnl_memory(conv3_weights.data(), conv3_user_weights_memory);
+    auto conv3_user_bias_memory = memory({{conv3_bias_tz}, dt::f32, tag::x}, eng);
+    write_to_dnnl_memory(conv3_bias.data(), conv3_user_bias_memory);
+
+    auto conv3_src_md = memory::desc({conv3_src_tz}, dt::f32, tag::any);
+    auto conv3_bias_md = memory::desc({conv3_bias_tz}, dt::f32, tag::any);
+    auto conv3_weights_md = memory::desc({conv3_weights_tz}, dt::f32, tag::any);
+    auto conv3_dst_md = memory::desc({conv3_dst_tz}, dt::f32, tag::any);
+
+    auto conv3_desc = convolution_forward::desc(prop_kind::forward,
+            algorithm::convolution_direct, conv3_src_md, conv3_weights_md,
+            conv3_bias_md, conv3_dst_md, conv3_strides, conv3_padding,
+            conv3_padding);
+    auto conv3_pd = convolution_forward::primitive_desc(conv3_desc, eng);
+
+    // create reorder primitives between user input and conv src if needed
+    auto conv3_src_memory = pool2_dst_memory;
+    if (conv3_pd.src_desc() != conv3_src_memory.get_desc()) {
+        conv3_src_memory = memory(conv3_pd.src_desc(), eng);
+        net_fwd.push_back(reorder(pool2_dst_memory, conv3_src_memory));
+        net_fwd_args.push_back({{DNNL_ARG_FROM, pool2_dst_memory},
+                {DNNL_ARG_TO, conv3_src_memory}});
+    }
+
+    auto conv3_weights_memory = conv3_user_weights_memory;
+    if (conv3_pd.weights_desc() != conv3_user_weights_memory.get_desc()) {
+        conv3_weights_memory = memory(conv3_pd.weights_desc(), eng);
+        net_fwd.push_back(
+                reorder(conv3_user_weights_memory, conv3_weights_memory));
+        net_fwd_args.push_back({{DNNL_ARG_FROM, conv3_user_weights_memory},
+                {DNNL_ARG_TO, conv3_weights_memory}});
+    }
+
+    auto conv3_bias_memory = conv3_user_bias_memory;
+    if (conv3_pd.bias_desc() != conv3_user_bias_memory.get_desc()) {
+        conv3_bias_memory = memory(conv3_pd.bias_desc(), eng);
+        net_fwd.push_back(
+            reorder(conv3_user_bias_memory, conv3_bias_memory)
+        );
+        net_fwd_args.push_back({{DNNL_ARG_FROM, conv3_user_bias_memory},
+                {DNNL_ARG_TO, conv3_bias_memory}});
+    }
+
+    // create memory for conv dst
+    auto conv3_dst_memory = memory(conv3_pd.dst_desc(), eng);
+
+    // finally create a convolution primitive
+    net_fwd.push_back(convolution_forward(conv3_pd));
+    net_fwd_args.push_back({{DNNL_ARG_SRC, conv3_src_memory},
+            {DNNL_ARG_WEIGHTS, conv3_weights_memory},
+            {DNNL_ARG_BIAS, conv3_bias_memory},
+            {DNNL_ARG_DST, conv3_dst_memory}});
+
+    // VGG11: block3: ReLU3
+    auto relu3_desc = eltwise_forward::desc(prop_kind::forward,
+            algorithm::eltwise_relu, conv3_dst_memory.get_desc(),
+            negative_slope);
+    auto relu3_pd = eltwise_forward::primitive_desc(relu3_desc, eng);
+
+    // create relu dst memory
+    auto relu3_dst_memory = memory(relu3_pd.dst_desc(), eng);
+
+    net_fwd.push_back(eltwise_forward(relu3_pd));
+    net_fwd_args.push_back({{DNNL_ARG_FROM, conv3_dst_memory},
+            {DNNL_ARG_TO, relu3_dst_memory}});
+
+    // VGG11: block3: conv4
+    // {batch, 256, 56, 56} -> {batch, 256, 56, 56}
+    memory::dims conv4_src_tz = {N, 256, 56, 56};
+    memory::dims conv4_weights_tz = {256, 256, 3, 3};
+    memory::dims conv4_bias_tz = {256};
+    memory::dims conv4_dst_tz = {N, 256, 56, 56};
+    memory::dims conv4_strides = {1, 1};
+    memory::dims conv4_padding = {1, 1};
+
+    std::vector<float> conv4_weights(product(conv4_weights_tz));
+    std::vector<float> conv4_bias(product(conv4_bias_tz));
+
+    // initializing non-zero values for weights and bias
+    for (size_t i = 0; i < conv4_weights.size(); ++i)
+        conv4_weights[i] = sinf((float)i);
+    for (size_t i = 0; i < conv4_bias.size(); ++i)
+        conv4_bias[i] = sinf((float)i);
+
+    // create memory for user data
+    auto conv4_user_weights_memory
+            = memory({{conv4_weights_tz}, dt::f32, tag::oihw}, eng);
+    write_to_dnnl_memory(conv4_weights.data(), conv4_user_weights_memory);
+    auto conv4_user_bias_memory = memory({{conv4_bias_tz}, dt::f32, tag::x}, eng);
+    write_to_dnnl_memory(conv4_bias.data(), conv4_user_bias_memory);
+
+    auto conv4_src_md = memory::desc({conv4_src_tz}, dt::f32, tag::any);
+    auto conv4_bias_md = memory::desc({conv4_bias_tz}, dt::f32, tag::any);
+    auto conv4_weights_md = memory::desc({conv4_weights_tz}, dt::f32, tag::any);
+    auto conv4_dst_md = memory::desc({conv4_dst_tz}, dt::f32, tag::any);
+
+    auto conv4_desc = convolution_forward::desc(prop_kind::forward,
+            algorithm::convolution_direct, conv4_src_md, conv4_weights_md,
+            conv4_bias_md, conv4_dst_md, conv4_strides, conv4_padding,
+            conv4_padding);
+    auto conv4_pd = convolution_forward::primitive_desc(conv4_desc, eng);
+
+    // create reorder primitives between user input and conv src if needed
+    auto conv4_src_memory = relu3_dst_memory;
+    if (conv4_pd.src_desc() != conv4_src_memory.get_desc()) {
+        conv4_src_memory = memory(conv4_pd.src_desc(), eng);
+        net_fwd.push_back(reorder(relu3_dst_memory, conv4_src_memory));
+        net_fwd_args.push_back({{DNNL_ARG_FROM, relu3_dst_memory},
+                {DNNL_ARG_TO, conv4_src_memory}});
+    }
+
+    auto conv4_weights_memory = conv4_user_weights_memory;
+    if (conv4_pd.weights_desc() != conv4_user_weights_memory.get_desc()) {
+        conv4_weights_memory = memory(conv4_pd.weights_desc(), eng);
+        net_fwd.push_back(
+                reorder(conv4_user_weights_memory, conv4_weights_memory));
+        net_fwd_args.push_back({{DNNL_ARG_FROM, conv4_user_weights_memory},
+                {DNNL_ARG_TO, conv4_weights_memory}});
+    }
+
+    auto conv4_bias_memory = conv4_user_bias_memory;
+    if (conv4_pd.bias_desc() != conv4_user_bias_memory.get_desc()) {
+        conv4_bias_memory = memory(conv4_pd.bias_desc(), eng);
+        net_fwd.push_back(
+            reorder(conv4_user_bias_memory, conv4_bias_memory)
+        );
+        net_fwd_args.push_back({{DNNL_ARG_FROM, conv4_user_bias_memory},
+                {DNNL_ARG_TO, conv4_bias_memory}});
+    }
+
+    // create memory for conv dst
+    auto conv4_dst_memory = memory(conv4_pd.dst_desc(), eng);
+
+    // finally create a convolution primitive
+    net_fwd.push_back(convolution_forward(conv4_pd));
+    net_fwd_args.push_back({{DNNL_ARG_SRC, conv4_src_memory},
+            {DNNL_ARG_WEIGHTS, conv4_weights_memory},
+            {DNNL_ARG_BIAS, conv4_bias_memory},
+            {DNNL_ARG_DST, conv4_dst_memory}});
+
+    // VGG11: block3: ReLU4
+    auto relu4_desc = eltwise_forward::desc(prop_kind::forward,
+            algorithm::eltwise_relu, conv4_dst_memory.get_desc(),
+            negative_slope);
+    auto relu4_pd = eltwise_forward::primitive_desc(relu4_desc, eng);
+
+    // create relu dst memory
+    auto relu4_dst_memory = memory(relu4_pd.dst_desc(), eng);
+
+    net_fwd.push_back(eltwise_forward(relu4_pd));
+    net_fwd_args.push_back({{DNNL_ARG_FROM, conv4_dst_memory},
+            {DNNL_ARG_TO, relu4_dst_memory}});
+
+    // VGG11: block3: max_pooling3
+    // {batch, 256, 56, 56} -> {batch, 256, 28, 28}
+    // kernel: {2, 2}; strides: {2, 2}; padding: {1, 1}
+    memory::dims pool3_dst_tz = {N, 256, 28, 28};
+    memory::dims pool3_kernel = {2, 2};
+    memory::dims pool3_strides = {2, 2};
+    memory::dims pool3_padding = {0, 0};
+
+    auto pool3_dst_md = memory::desc({pool3_dst_tz}, dt::f32, tag::any);
+
+    //[Create pooling primitive]
+    auto pool3_desc = pooling_forward::desc(prop_kind::forward,
+            algorithm::pooling_max, relu4_dst_memory.get_desc(), pool3_dst_md,
+            pool3_strides, pool3_kernel, pool3_padding, pool3_padding);
+    auto pool3_pd = pooling_forward::primitive_desc(pool3_desc, eng);
+    auto pool3_dst_memory = memory(pool3_pd.dst_desc(), eng);
+    //[Create pooling primitive]
+
+    // create pooling workspace memory if training
+    auto pool3_workspace_memory = memory(pool3_pd.workspace_desc(), eng);
+
+    net_fwd.push_back(pooling_forward(pool3_pd));
+    net_fwd_args.push_back({{DNNL_ARG_SRC, relu4_dst_memory},
+            {DNNL_ARG_DST, pool3_dst_memory}, // delay putting DST until reorder (if needed)
+            {DNNL_ARG_WORKSPACE, pool3_workspace_memory}});
+
+    // VGG11: block4: conv5
+    // {batch, 256, 28, 28} -> {batch, 512, 28, 28}
+    // kernel: {3, 3}; strides: {1, 1}; padding: {1, 1}
+    memory::dims conv5_src_tz = {N, 256, 28, 28};
+    memory::dims conv5_weights_tz = {512, 256, 3, 3};
+    memory::dims conv5_bias_tz = {512};
+    memory::dims conv5_dst_tz = {N, 512, 28, 28};
+    memory::dims conv5_strides = {1, 1};
+    memory::dims conv5_padding = {1, 1};
+
+    std::vector<float> conv5_weights(product(conv5_weights_tz));
+    std::vector<float> conv5_bias(product(conv5_bias_tz));
+
+    // initializing non-zero values for weights and bias
+    for (size_t i = 0; i < conv5_weights.size(); ++i)
+        conv5_weights[i] = sinf((float)i);
+    for (size_t i = 0; i < conv5_bias.size(); ++i)
+        conv5_bias[i] = sinf((float)i);
+
+    // create memory for user data
+    auto conv5_user_weights_memory
+            = memory({{conv5_weights_tz}, dt::f32, tag::oihw}, eng);
+    write_to_dnnl_memory(conv5_weights.data(), conv5_user_weights_memory);
+    auto conv5_user_bias_memory = memory({{conv5_bias_tz}, dt::f32, tag::x}, eng);
+    write_to_dnnl_memory(conv5_bias.data(), conv5_user_bias_memory);
+
+    auto conv5_src_md = memory::desc({conv5_src_tz}, dt::f32, tag::any);
+    auto conv5_bias_md = memory::desc({conv5_bias_tz}, dt::f32, tag::any);
+    auto conv5_weights_md = memory::desc({conv5_weights_tz}, dt::f32, tag::any);
+    auto conv5_dst_md = memory::desc({conv5_dst_tz}, dt::f32, tag::any);
+
+    auto conv5_desc = convolution_forward::desc(prop_kind::forward,
+            algorithm::convolution_direct, conv5_src_md, conv5_weights_md,
+            conv5_bias_md, conv5_dst_md, conv5_strides, conv5_padding,
+            conv5_padding);
+    auto conv5_pd = convolution_forward::primitive_desc(conv5_desc, eng);
+
+    // create reorder primitives between user input and conv src if needed
+    auto conv5_src_memory = pool3_dst_memory;
+    if (conv5_pd.src_desc() != conv5_src_memory.get_desc()) {
+        conv5_src_memory = memory(conv5_pd.src_desc(), eng);
+        net_fwd.push_back(reorder(pool3_dst_memory, conv5_src_memory));
+        net_fwd_args.push_back({{DNNL_ARG_FROM, pool3_dst_memory},
+                {DNNL_ARG_TO, conv5_src_memory}});
+    }
+
+    auto conv5_weights_memory = conv5_user_weights_memory;
+    if (conv5_pd.weights_desc() != conv5_user_weights_memory.get_desc()) {
+        conv5_weights_memory = memory(conv5_pd.weights_desc(), eng);
+        net_fwd.push_back(
+                reorder(conv5_user_weights_memory, conv5_weights_memory));
+        net_fwd_args.push_back({{DNNL_ARG_FROM, conv5_user_weights_memory},
+                {DNNL_ARG_TO, conv5_weights_memory}});
+    }
+
+    auto conv5_bias_memory = conv5_user_bias_memory;
+    if (conv5_pd.bias_desc() != conv5_user_bias_memory.get_desc()) {
+        conv5_bias_memory = memory(conv5_pd.bias_desc(), eng);
+        net_fwd.push_back(
+            reorder(conv5_user_bias_memory, conv5_bias_memory)
+        );
+        net_fwd_args.push_back({{DNNL_ARG_FROM, conv5_user_bias_memory},
+                {DNNL_ARG_TO, conv5_bias_memory}});
+    }
+
+    // create memory for conv dst
+    auto conv5_dst_memory = memory(conv5_pd.dst_desc(), eng);
+
+    // finally create a convolution primitive
+    net_fwd.push_back(convolution_forward(conv5_pd));
+    net_fwd_args.push_back({{DNNL_ARG_SRC, conv5_src_memory},
+            {DNNL_ARG_WEIGHTS, conv5_weights_memory},
+            {DNNL_ARG_BIAS, conv5_bias_memory},
+            {DNNL_ARG_DST, conv5_dst_memory}});
 
     return;
 }
