2fe1afa5e9db3ca975398597962af6422781b55a
Eitheages <Eitheages725@sjtu.edu.cn>
2022-09-08 18:17:36 +0800

    update

--- LeNet.cpp
+++ LeNet.cpp
@@ -1,32 +1,47 @@
 #include <opencv2/opencv.hpp>
+#include "layers.hpp"
 #include "mnist/mnist_reader.hpp"
-#include "new_layers.hpp"
-
 // #define DEBUG
 
 const std::string MNIST_DATA_LOCATION =
     "/home/cauchy/github/mnist-fashion/data/mnist";
 const std::string MNIST_FASHION_DATA_LOCATION =
     "/home/cauchy/github/mnist-fashion/data/fashion";
-
-// get fasion-mnist
-mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
-    mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(
-        MNIST_DATA_LOCATION, 0, 40);
+using type_dataset =
+    mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t>;
 
 // hyper parameter
 const int N = 16;  // batch_size
-const int step = 50;
+const int step = 100;
 float LearningRate =
-    0.1;  // replace with a lower number after serveral training
-const int epoch = 40;
+    0.01;  // replace with a lower number after serveral training
+const size_t epoch = 40;
+const int test_cnt = 400;
+const memory::dims inputs = {1, 28, 28};  // net input pic
+
+void get_data(const type_dataset& dataset, int& t, std::vector<float>& src,
+              std::vector<float>& dst,
+              const memory::dims& resized = {1, 28, 28},
+              std::string data_type = "train", bool normalized = false);
 
 void LeNet(engine::kind engine_kind) {
-    // Vectors of input data and expected output
-    std::vector<float> net_src(N * 1 * 28 * 28);
-    std::vector<float> net_dst(N * 10);  // 10 classes
 
-    const int IS = 28 * 28;
+    if (60000 < N * step || 10000 < test_cnt) {
+        std::cout << "there are not so much data!" << std::endl;
+        return;
+    }
+
+    // get mnist
+    type_dataset dataset =
+        mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(
+            MNIST_DATA_LOCATION, N * step, test_cnt);
+
+    std::cout << "Train data size: " << dataset.training_images.size() << " "
+              << "Test data size: " << dataset.test_images.size() << std::endl;
+
+    // Vectors of input data and expected output
+    std::vector<float> net_src(N * product(inputs));
+    std::vector<float> net_dst(N * 10);  // 10 classes
 
     auto eng = engine(engine_kind, 0);
     stream s(eng);
@@ -39,12 +54,12 @@ void LeNet(engine::kind engine_kind) {
     // LeNet conv1
     // {batch, 1, 28, 28} (x) {6, 1, 5, 5} -> {batch, 6, 28, 28}
     // padding = "same"
-    memory::dims conv1_src_tz = {N, 1, 28, 28};
+    memory::dims conv1_src_tz = {N, inputs[0], inputs[1], inputs[2]};
 
     auto conv1_src_memory = memory({{conv1_src_tz}, dt::f32, tag::nchw}, eng);
 
-    Conv2D conv1(N, 28, 6, 5, 1, 2, 0, conv1_src_memory, net_fwd, net_fwd_args,
-                 eng);
+    Conv2D conv1(N, conv1_src_tz[3], 6, 5, 1, 2, 0, conv1_src_memory, net_fwd,
+                 net_fwd_args, eng);
     Eltwise sigmoid1(algorithm::eltwise_logistic, 0.f, 0.f, conv1.arg_dst,
                      net_fwd, net_fwd_args, eng);
 
@@ -52,7 +67,7 @@ void LeNet(engine::kind engine_kind) {
     // {batch, 6, 28, 28} -> {batch, 6, 14, 14}
     // kernel = {2, 2}, strides = {2, 2}, padding = "valid"
 
-    MaxPool2D pool1(2, 2, sigmoid1.arg_dst, net_fwd, net_fwd_args, eng);
+    MeanPool2D pool1(2, 2, sigmoid1.arg_dst, net_fwd, net_fwd_args, eng);
 
     // LeNet conv2
     // {batch, 6, 14, 14} (x) {16, 6, 5, 5} -> {batch, 16, 10, 10}
@@ -66,7 +81,7 @@ void LeNet(engine::kind engine_kind) {
     // {batch, 16, 10, 10} -> {batch, 16, 5, 5}
     // kernel = {2, 2}, strides = {2, 2}, padding = "valid"
 
-    MaxPool2D pool2(2, 2, sigmoid2.arg_dst, net_fwd, net_fwd_args, eng);
+    MeanPool2D pool2(2, 2, sigmoid2.arg_dst, net_fwd, net_fwd_args, eng);
 
     // LeNet fc1
     // {batch, 16, 5, 5} -> {batch, 120}
@@ -172,8 +187,8 @@ void LeNet(engine::kind engine_kind) {
                                   net_bwd_args, eng);
 
     // pool2 back
-    MaxPool2D_back pool2_back(2, 2, pool2, fc1_back_data.arg_diff_src, net_bwd,
-                              net_bwd_args, eng);
+    MeanPool2D_back pool2_back(2, 2, pool2, fc1_back_data.arg_diff_src, net_bwd,
+                               net_bwd_args, eng);
 
     // conv2 back
     Eltwise_back sigmoid2_back(algorithm::eltwise_logistic, 0.f, 0.f, sigmoid2,
@@ -184,8 +199,8 @@ void LeNet(engine::kind engine_kind) {
                                      net_bwd, net_bwd_args, eng);
 
     // pool1 back
-    MaxPool2D_back pool1_back(2, 2, pool1, conv2_back_data.arg_diff_src,
-                              net_bwd, net_bwd_args, eng);
+    MeanPool2D_back pool1_back(2, 2, pool1, conv2_back_data.arg_diff_src,
+                               net_bwd, net_bwd_args, eng);
 
     // conv1 back
     Eltwise_back sigmoid1_back(algorithm::eltwise_logistic, 0.f, 0.f, sigmoid1,
@@ -233,39 +248,28 @@ void LeNet(engine::kind engine_kind) {
     // data index
 
     // --------------------- training ----------------------------
-    std::vector<float> entropy(step);
-    std::vector<float> loss(epoch);
+    int test_step = test_cnt / N;
+    std::vector<float> entropy(std::max(step, test_step));
+    std::vector<float> train_loss(epoch);
+    std::vector<float> test_loss(epoch);
 
     for (size_t k = 0; k < epoch; ++k) {
-        if ((k + 1) % 10 == 0) // update date learning rate every 10 training
+        if ((k + 1) % 10 == 0)  // update date learning rate every 10 training
             LearningRate /= 2;
-        std::cout << k + 1 << " th training..." << std::endl;
+        std::cout << std::left << std::setw(3) << k + 1 << "th training..."
+                  << std::endl;
         int train_t = 0;
         int test_t = 0;
-        int accurate_time = 0;
+        int train_accurate_time = 0;
+
+        // step
         for (size_t g = 0; g < step; ++g) {
-            //---------------- get input and output----------------
 
-            for (size_t i = 0; i < N * 10; ++i)
-                net_dst[i] = (float)0;
-
-            // read src and dst data from fasion-mnist
-            for (size_t i = 0; i < N; ++i) {
-                std::vector<uint8_t> pic = dataset.training_images[train_t];
-                size_t ans = dataset.training_labels[train_t];
-                ++train_t;
-
-                int fpi = i * 1 * IS;  // first pixel index
-
-                // write data into src while doing normalization (divided by 255)
-                for (size_t j = 0; j < IS; ++j)
-                    net_src[fpi + j] = ((float)pic[j]);
-
-                // write data into dst
-                net_dst[i * 10 + ans] = 1;
-            }
-
+            // get src and dst
+            get_data(dataset, train_t, net_src, net_dst, inputs, "train",
+                     false);
             write_to_dnnl_memory(net_src.data(), conv1_src_memory);
+
             // ---------------------------------------------------------------
             // forward calculate
             for (size_t i = 0; i < net_fwd.size(); ++i)
@@ -274,6 +278,8 @@ void LeNet(engine::kind engine_kind) {
             // calculate the loss function -- cross entropy
             read_from_dnnl_memory(y_hat.data(), softmax_dst_memory);
             read_from_dnnl_memory(y_hat_logged.data(), y_hat_logged_memory);
+
+            // three things are done below: calc loss, test accuracy, calc diff_softmax_src
             float crossEntropy = 0;
             size_t infer_ans = 0;
             for (size_t j = 0; j < y_hat_logged.size(); ++j) {
@@ -281,7 +287,7 @@ void LeNet(engine::kind engine_kind) {
                     infer_ans = j;
                 if ((j + 1) % 10 == 0) {
                     if (net_dst[infer_ans] > 0.5)
-                        ++accurate_time;
+                        ++train_accurate_time;
                     infer_ans = j + 1;
                 }
                 crossEntropy += y_hat_logged[j] * net_dst[j];
@@ -301,23 +307,70 @@ void LeNet(engine::kind engine_kind) {
             for (size_t i = 0; i < net_sgd.size(); ++i)
                 net_sgd.at(i).execute(s, net_sgd_args.at(i));
 
-            //[debug]
-            auto check_memory = conv1_back_weights.arg_diff_weights;
-            std::vector<float> check_data(
-                product(check_memory.get_desc().dims()));
-            read_from_dnnl_memory(check_data.data(), check_memory);
-            while (1)
-                break;
-            //[debug]
+            // [for_debug]
+            // auto check_memory = conv1_back_weights.arg_diff_weights;
+            // std::vector<float> check_data(
+            //     product(check_memory.get_desc().dims()));
+            // read_from_dnnl_memory(check_data.data(), check_memory);
+            // while (1)
+            //     break;
+            // [for_debug]
         }
 
-        float loss_sum = 0;
+        // calc avg_loss (train)
+        float train_loss_sum = 0;
         for (size_t g = 0; g < step; ++g)
-            loss_sum += entropy[g];
-        loss[k] = loss_sum / step;
-        std::cout << k + 1 << " th loss: " << loss[k]
-                  << ", accuracy: " << accurate_time / (float)(N * step)
-                  << std::endl;
+            train_loss_sum += entropy[g];
+        train_loss[k] = train_loss_sum / step;
+
+        // test the model
+        int test_accurate_time = 0;
+        for (size_t test_g = 0; test_g < test_step; ++test_g) {
+
+            // get src and dst
+            get_data(dataset, test_t, net_src, net_dst, inputs, "test", false);
+            write_to_dnnl_memory(net_src.data(), conv1_src_memory);
+
+            // forward calculate (inference)
+            for (size_t i = 0; i < net_fwd.size(); ++i)
+                net_fwd.at(i).execute(s, net_fwd_args.at(i));
+            read_from_dnnl_memory(y_hat.data(), softmax_dst_memory);
+            read_from_dnnl_memory(y_hat_logged.data(), y_hat_logged_memory);
+
+            // calc test_loss and test_accuracy
+            float crossEntropy = 0;
+            size_t infer_ans = 0;
+            for (size_t j = 0; j < y_hat_logged.size(); ++j) {
+                if (y_hat[infer_ans] < y_hat[j])
+                    infer_ans = j;
+                if ((j + 1) % 10 == 0) {
+                    if (net_dst[infer_ans] > 0.5)
+                        ++test_accurate_time;
+                    infer_ans = j + 1;
+                }
+                crossEntropy += y_hat_logged[j] * net_dst[j];
+            }
+            crossEntropy /= (float)(-N);
+            entropy[test_g] = crossEntropy;
+        }
+
+        // calc avg_test_loss
+        float test_loss_sum = 0;
+        for (size_t test_g = 0; test_g < test_step; ++test_g)
+            test_loss_sum += entropy[test_g];
+        test_loss[k] = train_loss_sum / test_step;
+
+        std::cout << std::left << std::setw(3) << k + 1
+                  << "train loss: " << std::fixed << std::setprecision(3)
+                  << train_loss[k] << ", train accuracy: " << std::fixed
+                  << std::setprecision(3)
+                  << train_accurate_time / (float)(N * step) << std::endl;
+        std::cout << std::left << std::setw(3) << ""
+                  << "test loss: " << std::fixed << std::setprecision(3)
+                  << test_loss[k] << ", test accuracy: " << std::fixed
+                  << std::setprecision(3)
+                  << test_accurate_time / (float)(test_cnt) << std::endl;
+        std::cout << std::endl;
     }
     // for (size_t k = 0; k < epoch; ++k)
     //     std::cout << k << "th training, loss: " << loss[k] << std::endl;
@@ -325,10 +378,57 @@ void LeNet(engine::kind engine_kind) {
 }
 
 int main(int argc, char** argv) {
-    if (dataset.training_images.size() < N * step) {
-        std::cout << "there are not so much data!" << std::endl;
-        std::cout << dataset.training_images.size() << std::endl;
-        return 0;
-    }
+
     return handle_example_errors(LeNet, parse_engine_kind(argc, argv));
+}
+
+void get_data(const type_dataset& dataset, int& t, std::vector<float>& src,
+              std::vector<float>& dst, const memory::dims& inputs,
+              std::string data_type, bool normalized) {
+    /**
+         * @brief get data from mnist data set
+         *
+         * @param dataset
+         * @param t the begining index
+         * @param src where the imagin data is written
+         * @param dst where the label data is written
+         * @param resized default {1, 28, 28}, it's highly recommended that (resize[0] == 1 or 3) and (resized[1] == resized[2])
+         * @param normalized default false, whether to normalize every pixel value to [0, 1]
+         */
+
+    //---------------- get input and output----------------
+    // initalize the output label
+    for (size_t i = 0; i < N * 10; ++i)
+        dst[i] = (float)0;
+
+    // read src and dst data from dataset
+    for (size_t i = 0; i < N; ++i) {
+        std::vector<uint8_t> pic;
+        size_t ans;
+        if (data_type == "train") {
+            pic = dataset.training_images[t];
+            ans = dataset.training_labels[t];
+        } else if (data_type == "test") {
+            pic = dataset.test_images[t];
+            ans = dataset.test_labels[t];
+        }
+        ++t;
+
+        size_t input_size = product(inputs) / inputs[0];
+        size_t channel = inputs[0];
+
+        size_t fpi = i * channel * input_size;  // first pixel index
+        bool if_resize = false;
+
+        // write data into src
+        if (normalized)
+            for (size_t j = 0; j < input_size; ++j)
+                src[fpi + j] = ((float)pic[j]) / 255.0;
+        else
+            for (size_t j = 0; j < input_size; ++j)
+                src[fpi + j] = ((float)pic[j]);
+
+        // write data into dst
+        dst[i * 10 + ans] = 1;
+    }
 }
